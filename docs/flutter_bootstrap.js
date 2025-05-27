27e53f961714798d771645595d353d12ceb94cd5
Prabesh Rai <raiprabesh789@gmail.com>
2025-05-27 00:17:20 -0400

    web_build/feat: experience and education section - 27-05-2025

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "692672970"
+    serviceWorkerVersion: "3236970207"
   }
 });
